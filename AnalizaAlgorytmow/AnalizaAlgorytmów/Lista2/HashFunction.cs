1edb70e26e9e266c745147a5c34e9d10f9ab427c
kamaz-home <kamaz08@gmail.com>
2018-03-21 18:39:23 +0100

    Revert "Revert "Merge branch 'test' into AnalizaAlgorytmow""
    This reverts commit 209214802bd9fa0cd63883e2e68b9fd2329c254f.

---
+++ AnalizaAlgorytmow/AnalizaAlgorytmów/Lista2/HashFunction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Numerics;
+using System.Linq;
+
+namespace Lista2
+{
+    public interface IHashFunction
+    {
+        BigInteger GetHash(int n);
+    }
+    public class HashFunction : IHashFunction
+    {
+        SHA256 _sHA256;
+        private static readonly BigInteger maks = new BigInteger(new byte[] { 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0 });
+        
+        public HashFunction()
+        {
+            _sHA256 = SHA256.Create();
+        }
+
+        public BigInteger GetHash(int n)
+        {
+            var bytes = BitConverter.GetBytes(n);
+            var hash = _sHA256.ComputeHash(bytes);
+            var bytelist = hash.ToList();
+            bytelist.Add(0);
+            return new BigInteger(bytelist.ToArray()); 
+        }
+
+        public static double Propability(BigInteger x)
+            => (double) maks / (double) x;
+    }
+}
---
+++ AnalizaAlgorytmow/AnalizaAlgorytmów/Lista2/Multiset.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2
+{
+    public abstract class MultisetAbstract<T>
+    {
+        protected List<List<T>> _multiSetList;
+        public MultisetAbstract()
+        {
+            _multiSetList = new List<List<T>>();
+        }
+
+        public int GetLength()
+        {
+            return _multiSetList.Count();
+        }
+
+        public List<T> GetMultiset(int n)
+        {
+            return _multiSetList[n];
+        }
+    }
+
+    public class GoodMultiset : MultisetAbstract<int>
+    {
+        public GoodMultiset(int begin, int end) : base()
+        {
+            for (int i = begin; i <= end; i++)
+                _multiSetList.Add(Enumerable.Range(((begin + i - 1) * (i - 1)) / 2, i).ToList());
+        }
+    }
+
+    public class BadMultiset : MultisetAbstract<int>
+    {
+        public BadMultiset(int begin, int end) : base()
+        {
+            for (int i = begin; i <= end; i++)
+                _multiSetList.Add(Enumerable.Range(begin, i).ToList());
+        }
+    }
+}
