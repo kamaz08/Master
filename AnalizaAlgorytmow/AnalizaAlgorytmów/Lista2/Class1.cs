cd9c7bcc4ef59173cf77d1958320e1c431939068
kamaz-home <kamaz08@gmail.com>
2018-03-21 18:39:50 +0100

    Revert "Revert "Revert "Revert "Merge branch 'test' into AnalizaAlgorytmow""""
    This reverts commit c3958ebb52a257c972fc4acf58ddd276d4ef5a2c.

---
+++ AnalizaAlgorytmow/AnalizaAlgorytmów/Lista2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            new HashFunction().GetHash(1);
+        }
+    }
+}
